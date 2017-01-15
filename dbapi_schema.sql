f569ed5ec88cf91ebac925a6f3e22ab80d5ad534
Ivan <romvano@gmail.com>
2017-01-15 15:52:52 +0300

    schema dumped

---
+++ dbapi_schema.sql
@@ -0,0 +1,174 @@
+-- MySQL dump 10.13  Distrib 5.5.53, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: dbapi
+-- ------------------------------------------------------
+-- Server version	5.5.53-0+deb8u1-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Follower`
+--
+
+DROP TABLE IF EXISTS `Follower`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Follower` (
+  `name` varchar(45) NOT NULL,
+  `followee` varchar(45) NOT NULL,
+  PRIMARY KEY (`name`,`followee`),
+  UNIQUE KEY `uk` (`name`,`followee`),
+  KEY `followee` (`followee`,`name`),
+  KEY `follower` (`name`,`followee`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Forum`
+--
+
+DROP TABLE IF EXISTS `Forum`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Forum` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `short_name` varchar(45) NOT NULL,
+  `user` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `short_name_unique` (`short_name`),
+  UNIQUE KEY `name_unique` (`name`),
+  KEY `user` (`user`)
+) ENGINE=InnoDB AUTO_INCREMENT=1228 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Post`
+--
+
+DROP TABLE IF EXISTS `Post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` datetime NOT NULL,
+  `thread` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `user` varchar(45) NOT NULL,
+  `forum` varchar(45) NOT NULL,
+  `parent` int(11) DEFAULT NULL,
+  `isApproved` tinyint(1) NOT NULL DEFAULT '0',
+  `isHighlighted` tinyint(1) NOT NULL DEFAULT '0',
+  `isEdited` tinyint(1) NOT NULL DEFAULT '0',
+  `isSpam` tinyint(1) NOT NULL DEFAULT '0',
+  `isDeleted` tinyint(1) NOT NULL DEFAULT '0',
+  `dislikes` int(11) NOT NULL DEFAULT '0',
+  `likes` int(11) NOT NULL DEFAULT '0',
+  `points` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `forum_date_id` (`forum`,`date`,`id`),
+  KEY `thread_date_id` (`thread`,`date`,`id`),
+  KEY `user_date_id` (`user`,`date`,`id`),
+  KEY `forum_user_id` (`forum`,`user`,`id`),
+  KEY `thread_user_id` (`thread`,`user`,`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1002044 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `PostHierarchy`
+--
+
+DROP TABLE IF EXISTS `PostHierarchy`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PostHierarchy` (
+  `post` int(11) NOT NULL,
+  `parent` int(11) NOT NULL DEFAULT '0',
+  `address` varchar(100) NOT NULL,
+  PRIMARY KEY (`parent`,`address`,`post`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Subscription`
+--
+
+DROP TABLE IF EXISTS `Subscription`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Subscription` (
+  `name` varchar(45) NOT NULL,
+  `thread` int(11) NOT NULL,
+  PRIMARY KEY (`name`,`thread`),
+  UNIQUE KEY `uk` (`name`,`thread`),
+  KEY `subscription` (`name`,`thread`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Thread`
+--
+
+DROP TABLE IF EXISTS `Thread`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Thread` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `forum` varchar(45) NOT NULL,
+  `title` varchar(45) NOT NULL,
+  `isClosed` tinyint(1) NOT NULL DEFAULT '0',
+  `user` varchar(45) NOT NULL,
+  `date` datetime NOT NULL,
+  `message` text NOT NULL,
+  `slug` varchar(45) NOT NULL,
+  `isDeleted` tinyint(1) NOT NULL DEFAULT '0',
+  `dislikes` int(11) NOT NULL DEFAULT '0',
+  `likes` int(11) NOT NULL DEFAULT '0',
+  `points` int(11) NOT NULL DEFAULT '0',
+  `posts` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `forum_date` (`forum`,`date`),
+  KEY `user_date` (`user`,`date`)
+) ENGINE=InnoDB AUTO_INCREMENT=12380 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `User`
+--
+
+DROP TABLE IF EXISTS `User`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `User` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) DEFAULT NULL,
+  `about` text,
+  `name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) NOT NULL,
+  `isAnonymous` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_unique` (`email`),
+  KEY `name_email` (`name`,`email`),
+  KEY `email_name` (`email`,`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=101009 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-01-15 15:52:34
